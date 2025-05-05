560533ba088b076d6236202f8bfb3a0ea1dd578b
Atlas <atlasmail@atlas.com>
2025-05-05 23:59:38 +0200

    1.5 counter is done

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,24 @@ function App() {
     return (
         <MainStyled>
             <MainBoxStyled>
+                <NumberFieldStyled>
+
+                </NumberFieldStyled>
+                <ButtonFieldStyled>
+                    <Button
+                    title={'set'}
+                    style={{
+                        color: '#292C35',
+                        backgroundColor: '#63DBFD',
+                        fontSize: '35px',
+                        padding: '5px',
+                        width: '150px',
+                        cursor: 'pointer'
+                    }}
+                    />
+                </ButtonFieldStyled>
+            </MainBoxStyled>
+            <MainBoxStyled>
                 <NumberFieldStyled counter={counter}>{counter}</NumberFieldStyled>
 
                 <ButtonFieldStyled>
@@ -71,7 +89,6 @@ function App() {
                                 color: '#292C35',
                                 backgroundColor: '#63DBFD',
                                 fontSize: '35px',
-                                marginRight: '20px',
                                 padding: '5px',
                                 width: '150px',
                                 cursor: counter >= 5 ? 'not-allowed' : 'pointer',
@@ -86,7 +103,6 @@ function App() {
                             color: '#292C35',
                             backgroundColor: '#63DBFD',
                             fontSize: '35px',
-                            marginRight: '20px',
                             padding: '5px',
                             width: '150px',
                             // cursor: 'pointer',
@@ -95,6 +111,7 @@ function App() {
                         }}/>
                 </ButtonFieldStyled>
             </MainBoxStyled>
+
         </MainStyled>
     )
 }
@@ -102,12 +119,18 @@ function App() {
 const MainStyled = styled.main`
     background-color: #292C35;
     height: 100%;
+    display: flex;
+    gap: 50px;
 `
 const MainBoxStyled = styled.div`
     background: none;
     border: 3px solid #63DBFD;
     padding: 20px;
     border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
 `
 type TypeProps = {
     color?: string
@@ -129,6 +152,10 @@ const ButtonFieldStyled = styled.div`
     border: 3px solid #63DBFD;
     border-radius: 15px;
     padding: 15px;
+    display: flex;             /* додаємо Flexbox */
+    justify-content: center;   /* центрує по горизонталі */
+    gap: 20px;                 /* відступ між кнопками */
+    flex-wrap: wrap;
 `
 
 
